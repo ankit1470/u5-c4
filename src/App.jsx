6c3d55be2414ef82a484a7179232a9a67ba06da9
Ankit sharma <sharmaankit1470@gmail.com>
2022-05-02 17:16:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { Home } from "./components/Home";
+import { Login } from "./components/Login";
+import "./App.css";
+import { useSelector } from "react-redux";
+import { Logout } from "./components/Logout";
+import { NewOrder } from "./components/NewOrder";
+import { Orders } from "./components/Orders";
+import { ProtectedRoute } from "./components/ProtextedRoute";
+import { Link, Routes, Route } from "react-router-dom";
+function App() {
+   const loginStatus = useSelector((store) => store.login);
+
+   return (
+      <div className="App">
+         <div className="navbar">
+            <Link className="nav-home" to="/">
+               Home
+            </Link>
+            {/* Show either login or logout below */}
+
+            {loginStatus ? (
+               <Link className="nav-logout" to="/logout">
+                  Logout
+               </Link>
+            ) : (
+               <Link className="nav-login" to="/login">
+                  Login
+               </Link>
+            )}
+         </div>
+
+         <Routes>
+           
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route
+               path="/neworder"
+               element={
+                  <ProtectedRoute>
+                     <NewOrder />
+                  </ProtectedRoute>
+               }
+            ></Route>
+            <Route
+               path="/orders"
+               element={
+                  <ProtectedRoute>
+                     <Orders />
+                  </ProtectedRoute>
+               }
+            ></Route>
+         </Routes>
+      </div>
+   );
+}
+
+export default App;
---
+++ src/Redux/Auth/reducer.js
@@ -0,0 +1,13 @@
+import { LOGIN_TOGGLE } from "./actions";
+
+export const reducer = (store, { type, payload }) => {
+   switch (type) {
+      case LOGIN_TOGGLE:
+         return {
+            ...store,
+            login: payload.login,
+         };
+      default:
+         return store;
+   }
+};
